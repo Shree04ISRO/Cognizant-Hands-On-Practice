c732e0b26f60f2ee69a9803bacd6dc2757ba12f0
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-06-16 23:07:36 +0530

    Add files via upload

---
+++ Module-6/TrainersMock.js
@@ -0,0 +1,30 @@
+import Trainer from './trainer';
+
+const trainers = [
+    new Trainer(
+        1,
+        "John",
+        "john@gmail.com",
+        "9876543210",
+        "React",
+        "JS, HTML, CSS"
+    ),
+    new Trainer(
+        2,
+        "David",
+        "david@gmail.com",
+        "9123456780",
+        "Java",
+        "Spring Boot"
+    ),
+    new Trainer(
+        3,
+        "Steve",
+        "steve@gmail.com",
+        "9988776655",
+        "Angular",
+        "TypeScript"
+    )
+];
+
+export default trainers;
