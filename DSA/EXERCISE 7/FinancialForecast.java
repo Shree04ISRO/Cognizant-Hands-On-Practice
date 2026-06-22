3e9de0e28357987e6a1a5f14a680fb691baf28d5
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-06-22 19:10:16 +0530

    Add files via upload

---
+++ DSA/EXERCISE 7/FinancialForecast.java
@@ -0,0 +1,18 @@
+public class FinancialForecast {
+
+    static double predictFutureValue(
+            double currentValue,
+            double growthRate,
+            int years) {
+
+        if(years == 0) {
+
+            return currentValue;
+        }
+
+        return predictFutureValue(
+                currentValue * (1 + growthRate),
+                growthRate,
+                years - 1);
+    }
+}
