13c43319c378547c632216cab226dafa74061843
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-06-22 22:36:41 +0530

    Add files via upload

---
+++ DESIGN AND PATTERNS/EXERCISE 5/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Notifier notifier = new EmailNotifier();
+
+        notifier = new SMSNotifierDecorator(notifier);
+        notifier = new SlackNotifierDecorator(notifier);
+
+        notifier.send("Your order has been shipped!");
+    }
+}
---
+++ DESIGN AND PATTERNS/EXERCISE 5/NotifierDecorator.java
@@ -0,0 +1,13 @@
+abstract class NotifierDecorator implements Notifier {
+
+    protected Notifier notifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.notifier = notifier;
+    }
+
+    @Override
+    public void send(String message) {
+        notifier.send(message);
+    }
+}
