f93e4dbd1584cdcf85c593bab86ed9d3ef6b2b28
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-06-28 00:32:28 +0530

    Add files via upload

---
+++ SPRING MAVEN/EXERCISE 3/BookService.java
@@ -0,0 +1,24 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository repository;
+
+    // Constructor Injection
+    public BookService(BookRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addBook() {
+
+        System.out.println("Book Service Started");
+
+        repository.saveBook();
+
+        System.out.println("Book Service Completed");
+
+    }
+
+}
