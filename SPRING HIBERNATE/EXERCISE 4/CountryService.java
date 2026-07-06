1e29c0cbab5768ef0f7a0ead358bce6c54268b1b
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-07-07 00:09:48 +0530

    Add files via upload

---
+++ SPRING HIBERNATE/EXERCISE 4/CountryService.java
@@ -0,0 +1,51 @@
+package com.cognizant.ormlearn.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    // Get all countries
+    @Transactional(readOnly = true)
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    // Search countries by partial name
+    @Transactional(readOnly = true)
+    public List<Country> searchCountries(String text) {
+        return countryRepository.findByNameContainingIgnoreCase(text);
+    }
+
+    // Find country by code
+    @Transactional(readOnly = true)
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+
+        Optional<Country> result = countryRepository.findById(countryCode);
+
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found: " + countryCode);
+        }
+
+        return result.get();
+    }
+
+    // Add a new country
+    @Transactional
+    public void addCountry(Country country) {
+        countryRepository.save(country);
+    }
+
+}
