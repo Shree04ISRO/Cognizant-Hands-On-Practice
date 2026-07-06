b3d659ad26adfd2ea0c7d1cf42024eb74ff64c38
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-07-06 23:55:16 +0530

    Add files via upload

---
+++ SPRING HIBERNATE/EXERCISE 3/OrmLearnApplication.java
@@ -0,0 +1,79 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    private static CountryService countryService;
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                SpringApplication.run(OrmLearnApplication.class, args);
+
+        countryService =
+                context.getBean(CountryService.class);
+
+        getAllCountriesTest();
+
+        searchCountriesTest();
+
+        getCountryTest();
+    }
+
+    private static void getAllCountriesTest() {
+
+        LOGGER.info("******** ALL COUNTRIES ********");
+
+        List<Country> countries =
+                countryService.getAllCountries();
+
+        countries.forEach(System.out::println);
+
+    }
+
+    private static void searchCountriesTest() {
+
+        LOGGER.info("******** SEARCH RESULT ********");
+
+        List<Country> countries =
+                countryService.searchCountries("Ind");
+
+        countries.forEach(System.out::println);
+
+    }
+
+    private static void getCountryTest() {
+
+        LOGGER.info("******** FIND COUNTRY ********");
+
+        try {
+
+            Country country =
+                    countryService.findCountryByCode("IN");
+
+            LOGGER.debug("Country : {}", country);
+
+        } catch (CountryNotFoundException e) {
+
+            LOGGER.error(e.getMessage());
+
+        }
+
+    }
+
+}
