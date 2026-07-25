6c103a7719c347ecdfa83e88e5963f4bdb978883
Shreesl040905 <vh12998_ai23@velhightech.com>
2026-07-26 00:42:30 +0530

    Add files via upload

---
+++ REACT/EXERCISE 7/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Home from './component/Home';
+import About from './component/About';
+import Contact from './component/Contact';
+
+function App() {
+    return ( <
+        div className = "container" >
+        <
+        Home / >
+        <
+        About / >
+        <
+        Contact / >
+        <
+        /div>
+    );
+}
+
+export default App;
